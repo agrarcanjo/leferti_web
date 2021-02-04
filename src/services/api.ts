3b6d4a25eca293509fdf32551a6146d407445677
agrarcanjo <agr.arcanjo@gmail.com>
2021-02-04 12:52:05 -0300

    ambiente de produçção

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   //baseURL: 'http://backend-dev22.us-east-2.elasticbeanstalk.com/' 
-  baseURL: 'http://3.15.140.207:8080'
+  baseURL: 'http://13.59.168.219:8080'
   //baseURL: 'http://192.168.100.127:8080'
 })
 
